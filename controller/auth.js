bfbd1c32ffcf8635ad2897583750dfa821705b04
han sheng <gh890528@gmail.com>
2023-09-17 12:36:01 +0800

    finish auth api

--- controller/auth.js
+++ controller/auth.js
@@ -1,8 +1,24 @@
 import {db} from '../connect.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const login = (req, res) => {
+    const q = "SELECT * FROM users WHERE username = ?"
 
+    db.query(q, [req.body.username], (err, data) => {
+        if(err) return res.status(500).json(err)
+        if (data.length === 0) return res.status(404).json('User not font')
+
+        const checkPassword = bcryptjs.compareSync(req.body.password, data[0].password)
+
+        if (!checkPassword) return res.status(400).json('Wrong password or name')
+
+        const token = jwt.sign({ id: data[0].id }, "secretkey")
+        const {password, ...others} = data[0]
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+        }).status(200).json(others)
+    })
 }
 
 export const register = (req, res) => {
@@ -32,5 +48,8 @@ export const register = (req, res) => {
 }
 
 export const logout = (req, res) => {
-    
+    res.clearCookie("accessToken", {
+        secure: true,
+        sameSate: "none"
+    }).status(200).json("User hs been logged out")
 }
--- index.js
+++ index.js
@@ -7,8 +7,12 @@ import userRoutes from './routes/user.js'
 import postRoutes from './routes/post.js'
 import commentRoutes from './routes/comment.js'
 import likeRoutes from './routes/like.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
