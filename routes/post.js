a43a8a7ace0853005a37d68d413ae08f211e17d4
han sheng <gh890528@gmail.com>
2023-09-24 14:59:44 +0800

    get posts data

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import {} from '../controller/post.js'
+import { getPosts } from '../controller/post.js'
 
 const router = express.Router()
 
-// router.get()
+router.get("/", getPosts)
 
 export default router
